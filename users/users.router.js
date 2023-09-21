3cd561ce24377d53a3c4f296463faef0a54ad48c
Mujeeb Adetokunbo <mujeeb_adetokunbo@mckinsey.com>
2023-09-21 17:36:59 +0100

    validation of request body using joi

--- users/users.router.js
+++ users/users.router.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const { createUser, loginUser } = require("./users.controller");
+const {
+  validateSignUp,
+  validateLogin,
+} = require("../middlewares/requestValidator");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.end("get all users");
 });
 
-router.post("/signup", createUser);
-router.post("/login", loginUser);
+router.post("/signup", validateSignUp, createUser);
+router.post("/login", validateLogin, loginUser);
 
 module.exports = router;
